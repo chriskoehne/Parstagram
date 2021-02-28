70afb0fc4b322bdf44ea2a682055fd4a6fe95e4b
Chris Koehne <cdkoehne@gmail.com>
2021-02-28 12:01:49 -0500

    added signup/login, making and viewing posts

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "hsXFyIABdRS1R0Lfe41sXrIu05l2tcpO7dfXHWbs"
+                    $0.clientKey = "vmapdfZUfslzvCSxHrgx451I3OTzXtU5lNX75k7A"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
